25bae79ce38991d3e4e5f2f57dc2599def6caef7
Mateusz <bmatteo@interia.pl>
2024-05-28 11:51:08 +0200

    update

--- src/Zadanie5.java
+++ src/Zadanie5.java
@@ -22,6 +22,15 @@ public class Zadanie5 {
     16 ..
      */
     public static void main(String[] args) {
-
+        for(int i = 1; i < 101; i++) {
+            System.out.print(i);
+            if(i % 3 == 0) {
+                System.out.print(" Fizz");
+            }
+            if(i % 5 == 0) {
+                System.out.print(" Buzz");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/Zadanie6.java
@@ -0,0 +1,7 @@
+public class Zadanie6 {
+    /*
+    wygeneruj tablice losowych (0 - 999) liczb 2D (10x10)
+    program ma policzyć średią każdego wiersza
+    program ma wypisać największą średnią i nuymer wiersza w którym ona wystąpiła
+     */
+}
